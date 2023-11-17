a21b52bb39a58b996f21db45b8ae5422728ca249
FcoEvanderson <f.evanderson13@gmail.com>
2023-11-17 10:16:39 -0300

    estrutura_dados171123

---
+++ arvores/arvore_binaria/bin_tree.py
@@ -0,0 +1,39 @@
+from queue import Queue
+
+
+class _BinTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def pre_order(self, subtree):
+        if subtree is not None:
+            print(subtree.data)
+            self.pre_order(subtree.left)
+            self.pre_order(subtree.right)
+
+    def in_order(self, subtree):
+        if subtree is not None:
+            self.in_order(subtree.left)
+            print(subtree.data)
+            self.in_order(subtree.right)
+
+    def post_order(self, subtree):
+        if subtree is not None:
+            self.post_order(subtree.left)
+            self.post_order(subtree.right)
+            print(subtree.data)
+
+    def breadthFirst(self, bintree):
+        q = Queue()
+        q.enqueue(bintree)
+        while not q is None:
+            node = q.dequeue()
+            print(node.data)
+            if node.left is not None:
+                q.enqueue(node.left)
+            if node.right is not None:
+                q.enqueue(node.right)
+
+    
---
+++ arvores/arvore_binaria/queue.py
@@ -0,0 +1,47 @@
+from node import Node
+
+
+class Queue:
+    def __init__(self):
+        self.first = None
+        self.last = None
+        self._size = 0
+
+    def enqueue(self, element):
+        node = Node(element)
+        if self.last is None:
+            self.last = node
+        else:
+            self.last.next = node
+            self.last = node
+        if self.first is None:
+            self.first = node
+        self._size += 1
+
+    def dequeue(self):
+        if self.first is not None:
+            element = self.first.data
+            self.first = self.first.next
+            self._size -= 1
+            return element
+        raise IndexError("The Queue is empty!")
+
+    def peek(self):
+        if self.first is not None:
+            element = self.first.data
+            return element
+        raise IndexError("The Queue is empty!")
+
+    def __len__(self): return self._size
+
+    def __repr__(self):
+        if self._size > 0:
+            r = ""
+            pointer = self.first
+            while(pointer):
+                r = r + str(pointer.data) + " "
+                pointer = pointer.next
+            return r
+        return "Empty Queue"
+
+    def __str__(self): return self.__repr__()
